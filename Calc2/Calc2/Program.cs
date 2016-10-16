1229917a67280ccb80ed000762396356a5c6aaac
SnowyTom <tanaka-tomoyuki-wy@ynu.jp>
2016-10-16 21:37:09 +0900

    初期リリース
    ただの電卓、括弧も使えない。

--- Calc2/Calc2/Program.cs
+++ Calc2/Calc2/Program.cs
@@ -12,8 +12,8 @@ namespace Calc2
         {
             string mathstring = null;
             Value value = new Value();
-            //mathstring = Console.ReadLine();
-            mathstring = "1+2+3*2+4-1";//for debug
+            mathstring = Console.ReadLine();
+            //mathstring = "1+2+3*2+4-1";//for debug
             long ans = value.devided(mathstring);
             Console.WriteLine(ans);
             Console.ReadLine();
